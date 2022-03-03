b1201a1171d139e95bc666e54df834095c996d07
poojapatel815 <ppate815@my.centennialcollege.ca>
2022-03-04 02:09:04 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,7 @@
 exports.home = function(req, res, next) {
     res.render('index', { title: 'Home' });
 };
+
+exports.about = function(req,res,next){
+    res.render('index', { title: 'About'});
+};
